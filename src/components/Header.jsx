b8656920416caa9399a3bbf266bb0c37c7340ccf
Vse Putyom <bertraid777@gmail.com>
2021-05-12 23:14:48 +0300

    test webStorm

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = () => {
-    return <header className='header'>
-        <img src='https://purepng.com/public/uploads/thumbnail//google-stadia-logo-hd4.png' alt='logo'></img>
-    </header>
-};
-
-export default Header;
